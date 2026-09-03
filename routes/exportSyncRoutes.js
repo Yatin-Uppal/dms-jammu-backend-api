886f7afda06c504907e31abdae4df03e4d28d62d
Prince Rawat <prince.rawat@talentelgia.com>
2026-09-03 10:34:42 +0530

    removed the auth middleware in the export/locations and export/data-sync api

--- routes/exportSyncRoutes.js
+++ routes/exportSyncRoutes.js
@@ -6,8 +6,8 @@ const exportSyncController = require("../controllers/exportSyncController");
 const { verifyAccessToken } = require("../middleware/authMiddleware");
 
 // Export gate sync endpoints
-router.get("/export/locations", verifyAccessToken, exportSyncController.getLocations);
-router.post("/export/data-sync", verifyAccessToken, exportSyncController.dataSync);
+router.get("/export/locations", exportSyncController.getLocations);
+router.post("/export/data-sync", exportSyncController.dataSync);
 router.get("/export/sync-history", verifyAccessToken, exportSyncController.getSyncHistory);
 
 module.exports = router;
